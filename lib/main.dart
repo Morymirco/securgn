6bb18954446f8a3c8887cf8123df1c663ec19ff2
Morymirco <morykoulibaly996@gmail.com>
2024-12-02 10:36:15 +0000

    'reussite'

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,39 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/firebase_options.dart';
 import 'package:my_app/screens/splash_screen.dart';
 import 'package:my_app/services/emergency_services_initializer.dart';
 
+// Gestionnaire de messages en arrière-plan
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  // Initialiser les services d'urgence
-  final servicesInitializer = EmergencyServicesInitializer();
   try {
-    await servicesInitializer.initializeEmergencyServices();
-    print('Services d\'urgence initialisés avec succès');
-  } catch (e) {
-    print('Erreur lors de l\'initialisation des services d\'urgence: $e');
-  }
+    WidgetsFlutterBinding.ensureInitialized();
+    
+    // Initialiser Firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  FlutterError.onError = (FlutterErrorDetails details) {
-    FlutterError.presentError(details);
-  };
-  runApp(const MyApp());
+ 
+
+  
+
+    // Initialiser les services d'urgence
+    final servicesInitializer = EmergencyServicesInitializer();
+    try {
+      await servicesInitializer.initializeEmergencyServices();
+      print('Services d\'urgence initialisés avec succès');
+    } catch (e) {
+      print('Erreur lors de l\'initialisation des services d\'urgence: $e');
+    }
+
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+    };
+    runApp(const MyApp());
+  } catch (e) {
+    print('Erreur d\'initialisation: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -29,12 +43,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SecurGuinee',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: const Color(0xFF094FC6),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF094FC6)),
+        useMaterial3: true,
       ),
       home: const SplashScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
